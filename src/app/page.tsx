c7732b7e9b8991d9cacfd99464eaf09ab9debb22
Salah akhsayem <salahakhsayem08@gmail.com>
2026-02-15 14:42:08 +0000

    bunu çok az çok ufak yukarı kaydır (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Navbar />
       
       {/* Hero Section - Cinematic Treatment */}
-      <section className="relative min-h-[95vh] flex flex-col items-center pt-60 md:pt-80 overflow-visible">
+      <section className="relative min-h-[95vh] flex flex-col items-center pt-52 md:pt-72 overflow-visible">
         <div className="absolute inset-0 z-0">
           <Image
             src={heroImage.imageUrl}
